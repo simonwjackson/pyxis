e0a486a3ea7d025cb242b479289ffe76f7dc3765
Simon W. Jackson <github@simonwjackson.io>
2025-12-04 19:45:25 -0700

    build: migrate from bun to npm and add nix package

--- flake.nix
+++ flake.nix
@@ -15,6 +15,45 @@
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.buildNpmPackage {
+            pname = "pyxis";
+            version = "0.1.0";
+            src = ./.;
+
+            npmDepsHash = "sha256-cdOKoKfGQarop5HHAQyyYLI1iZ2dk/H3M3TiAFVoUlc=";
+            makeCacheWritable = true;
+
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+
+            buildPhase = ''
+              runHook preBuild
+              npm run build
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out/lib/pyxis $out/bin
+              cp -r dist $out/lib/pyxis/
+              cp -r node_modules $out/lib/pyxis/
+              cp package.json $out/lib/pyxis/
+              makeWrapper ${pkgs.bun}/bin/bun $out/bin/pyxis \
+                --add-flags "$out/lib/pyxis/dist/cli/bin.js"
+              runHook postInstall
+            '';
+
+            meta = {
+              description = "Unofficial Pandora music service CLI client";
+              mainProgram = "pyxis";
+            };
+          };
+        });
+
       devShells = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
