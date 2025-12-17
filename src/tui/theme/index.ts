5320fe1ae11f311bc45cf934c956c6896c051c10
Simon W. Jackson <github@simonwjackson.io>
2025-12-17 16:33:42 -0700

    chore: remove bun.lock and update dependencies

--- src/tui/theme/index.ts
+++ src/tui/theme/index.ts
@@ -3,25 +3,22 @@ export { ThemeProvider, useTheme } from "./provider.js";
 export { loadTheme, getDefaultTheme, themeNames } from "./loader.js";
 
 // Re-export individual themes for direct import
-export { default as pyxisTheme } from "./themes/pyxis.json" with {
-	type: "json",
-};
-export { default as tokyonightTheme } from "./themes/tokyonight.json" with {
-	type: "json",
-};
-export { default as catppuccinTheme } from "./themes/catppuccin.json" with {
-	type: "json",
-};
-export { default as nordTheme } from "./themes/nord.json" with { type: "json" };
-export { default as gruvboxTheme } from "./themes/gruvbox.json" with {
-	type: "json",
-};
-export { default as draculaTheme } from "./themes/dracula.json" with {
-	type: "json",
-};
-export { default as rosePineTheme } from "./themes/rose-pine.json" with {
-	type: "json",
-};
-export { default as systemTheme } from "./themes/system.json" with {
-	type: "json",
+import pyxisTheme from "./themes/pyxis.json" with { type: "json" };
+import tokyonightTheme from "./themes/tokyonight.json" with { type: "json" };
+import catppuccinTheme from "./themes/catppuccin.json" with { type: "json" };
+import nordTheme from "./themes/nord.json" with { type: "json" };
+import gruvboxTheme from "./themes/gruvbox.json" with { type: "json" };
+import draculaTheme from "./themes/dracula.json" with { type: "json" };
+import rosePineTheme from "./themes/rose-pine.json" with { type: "json" };
+import systemTheme from "./themes/system.json" with { type: "json" };
+
+export {
+	pyxisTheme,
+	tokyonightTheme,
+	catppuccinTheme,
+	nordTheme,
+	gruvboxTheme,
+	draculaTheme,
+	rosePineTheme,
+	systemTheme,
 };
